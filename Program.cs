026525ce3fa0396f5cdb73a5a81cc549e7b3d713
Glenn <glennwiz@gmail.com>
2023-05-30 17:41:52 +0200

    Add debug mode option and refactor matrix operations
    This commit adds the option to run the program in debug mode. The user is prompted to choose whether to run in debug mode or not. If debug mode is selected, the program runs the matrix operations with debug output. If not, it runs the matrix operations without debug output.
    
    Here are the specific changes made:
    
    - Added a new method `AskForDebugMode()` to prompt the user for debug mode selection.
    - Created separate methods `RunWithDebug()` and `RunWithoutDebug()` to handle the matrix operations based on the debug mode selection.
    - Moved the original matrix initialization, SVD computation, and low-rank approximation code into the appropriate methods.
    - Adjusted the size of the U, Σ, and V* matrices to match the 100x100 matrix size.
    - Removed unnecessary blank lines and commented-out code.
    
    These changes enhance the flexibility of the program by allowing users to choose between debug mode and non-debug mode execution. The debug mode provides detailed output, while the non-debug mode runs the matrix operations without excessive console output.
    
    Please note that the provided code diff assumes the remaining parts of the code remain unchanged.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool runWithDebug = AskForDebugMode();
+
+        if (runWithDebug)
+        {
+            RunWithDebug();
+        }
+        else
+        {
+            RunWithoutDebug();
+        }
+    }
+
+    static bool AskForDebugMode()
+    {
+        Console.WriteLine("Do you want to run in debug mode? (y/n)");
+        string input = Console.ReadLine();
+
+        return (input.ToLower() == "y");
+    }
+
+    static void RunWithDebug()
+    {
         Console.WriteLine("Initializing a 100x100 matrix with random values between 0 and 1...");
         var rng = new System.Random();
         var matrix = Matrix<double>.Build.Dense(100, 100, (i, j) => rng.NextDouble());
         Console.WriteLine("Original Matrix (100x100 with random values):");
-        Console.WriteLine(matrix.ToString()); // This line can be commented out to prevent the matrix from filling the console output
+        Console.WriteLine(matrix.ToString());
 
         Console.WriteLine("Computing the Singular Value Decomposition (SVD) of the matrix...");
         var svd = matrix.Svd();
@@ -29,7 +51,6 @@ class Program
         Console.WriteLine("Creating a diagonal matrix with the first 'lowRank' singular values...");
         var sMatrix = Matrix<double>.Build.DenseDiagonal(lowRank, lowRank, i => svd.S[i]);
 
-        
         Console.WriteLine("\nU Matrix (first 5 columns):");
         Console.WriteLine(uMatrix.ToString());
 
@@ -38,12 +59,27 @@ class Program
 
         Console.WriteLine("\nV Transposed Matrix (first 5 rows):");
         Console.WriteLine(vTMatrix.ToString());
-       
 
         Console.WriteLine("Computing the low-rank approximation by multiplying the U, Σ, and V* matrices together...");
         var lowRankMatrix = uMatrix * sMatrix * vTMatrix;
 
         Console.WriteLine("\nLow Rank Approximation:");
-        Console.WriteLine(lowRankMatrix.ToString()); // Commented out to prevent massive output. Uncomment if you want to print the matrix
+        Console.WriteLine(lowRankMatrix.ToString());
+    }
+
+    static void RunWithoutDebug()
+    {
+        var rng = new System.Random();
+        var matrix = Matrix<double>.Build.Dense(100, 100, (i, j) => rng.NextDouble());
+
+        var svd = matrix.Svd();
+
+        int lowRank = 5;
+
+        var uMatrix = svd.U.SubMatrix(0, 100, 0, lowRank); // Adjusted size for 100x100 matrix
+        var vTMatrix = svd.VT.SubMatrix(0, lowRank, 0, 100); // Adjusted size for 100x100 matrix
+        var sMatrix = Matrix<double>.Build.DenseDiagonal(lowRank, lowRank, i => svd.S[i]);
+
+        var lowRankMatrix = uMatrix * sMatrix * vTMatrix;
     }
 }
